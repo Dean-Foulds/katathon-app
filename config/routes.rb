1ec64500339cfa2b946244f7053d9f50bf045b99
deanfoulds <deanfoulds@gmail.com>
2016-11-27 10:54:50 +0000

    First version for push to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'visitors#home'
+  get '/index', to: 'visitors#index', as: :index
+  get '/gallery', to: 'pages#gallery', as: :gallery
+  get '/about', to: 'pages#about', as: :about
   devise_for :users
   resources :users
 end
